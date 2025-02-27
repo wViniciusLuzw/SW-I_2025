744361f983118122f42ef484da448068b5a0b282
wViniciusLuzw <vinicius.luz.etec@gmail.com>
2025-02-27 11:04:51 -0300

    1104

---
+++ aula_03/Exer/exerProcess.php
@@ -0,0 +1,7 @@
+<?php
+    $nome = $_POST['cxnome'];
+    $mensagem = $_POST['cxmensagem'];
+
+    echo "<p>Nome do Usuário: $nome</p>";
+    echo "<p>Mensagem Digitada: $mensagem</p>";
+?>
--- aula_03/processa01.php
+++ aula_03/processa01.php
@@ -3,5 +3,5 @@
     $idade = $_GET['cxidade']; //$_POST => $_GET
 
     echo "<p>Nome digitado: $nome</p>";
-    echo "<p>Idade Digitado: $idade anos de vida!</p>"
+    echo "<p>Idade Digitado: $idade anos de vida!</p>";
 ?>
