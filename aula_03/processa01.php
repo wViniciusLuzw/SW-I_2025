c8933e44ab48e1be59db5f0a8aee858bea561ccb
wViniciusLuzw <vinicius.luz.etec@gmail.com>
2025-02-27 10:43:00 -0300

    1042

--- aula_03/processa01.php
+++ aula_03/processa01.php
@@ -1,9 +1,7 @@
 <?php
-    $nome = $_POST['cxnome'];
-    $idade = $_POST['cxidade'];
+    $nome = $_GET['cxnome']; //$_POST => $_GET
+    $idade = $_GET['cxidade']; //$_POST => $_GET
 
     echo "<p>Nome digitado: $nome</p>";
     echo "<p>Idade Digitado: $idade anos de vida!</p>"
-
-
 ?>
