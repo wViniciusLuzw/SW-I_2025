5ad7adfbb92a7535ab3203450454ee3ffadadfb7
wViniciusLuzw <vinicius.luz.etec@gmail.com>
2025-05-23 18:06:03 -0300

    1805

--- aula_12/Exer/exProdutos.php
+++ aula_12/Exer/exProdutos.php
@@ -2,7 +2,11 @@
 
     include_once 'Produto.class.php';
 
-    $produto1 = new Produto("Tamires", 15.00);
+    $nome = $_POST['nome'];
+    $valor = $_POST['valor'];
+    $quant = $_POST['quant'];
+
+    $produto1 = new Produto($nome, $valor, $quant);
     $produto1->adicionarEstoque(5);
     $produto1->removerEstoque(3);
     $produto1->mostrarDetalhes();
